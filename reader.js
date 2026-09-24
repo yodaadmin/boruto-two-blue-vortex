2211f9b6f13ebfaa3ff24bc294643a7d6b2cd701
yodaadmin <yodaedit7@gmail.com>
2026-09-25 00:29:18 +0300

    Delete reader.js

--- reader.js
+++
@@ -1,4 +0,0 @@
-const params=new URLSearchParams(location.search),num=params.get('chapter')||'1';document.querySelector('#title').textContent='الفصل '+num;
-document.querySelector('#theme').onclick=()=>document.body.classList.toggle('light');
-const pages=JSON.parse(localStorage.getItem('chapter_pages_'+num)||'[]');const box=document.querySelector('#pages');
-if(pages.length){box.innerHTML=pages.map(src=>`<img class="page" src="${src}" loading="lazy">`).join('')}
